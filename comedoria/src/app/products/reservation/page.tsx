774e9b6bdea71c923f932fb483b872fc7c669295
Day Lima <dayanecamilelima@gmail.com>
2024-10-07 12:39:58 -0300

    deploy correction

--- comedoria/src/app/products/reservation/page.tsx
+++ comedoria/src/app/products/reservation/page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import CartItemDisplay from "./sections/CartItemDisplay/cartItemDisplay";
 import TotalDisplay from "./sections/TotalDisplay/totalDisplay";
 import { useCart } from "./sections/useCartHook/useCart";
+import { Suspense } from "react"; // Importando Suspense
 
 const FinalizeReservation = () => {
   const { cartItems, calculateTotal } = useCart();
@@ -24,33 +25,35 @@ const FinalizeReservation = () => {
         <PriceBanner />
       </div>
       <main className="flex-grow container mx-auto px-4 py-8">
-        <h2 className="rubik-700 text-4xl font-bold text-[#45480F]  mb-8 sm:text-left">
+        <h2 className="rubik-700 text-4xl font-bold text-[#45480F] mb-8 sm:text-left">
           Finalize sua reserva
         </h2>
-        {cartItems.length > 0 ? (
-          <div className="flex flex-col mx-auto max-w-full md:max-w-[640px]">
-            <div className="space-y-4 mb-4">
-              {cartItems.map(item => (
-                <CartItemDisplay key={item.product_name} item={item} />
-              ))}
+        <Suspense fallback={<div>Loading...</div>}> {/* Boundary de Suspense */}
+          {cartItems.length > 0 ? (
+            <div className="flex flex-col mx-auto max-w-full md:max-w-[640px]">
+              <div className="space-y-4 mb-4">
+                {cartItems.map(item => (
+                  <CartItemDisplay key={item.product_name} item={item} />
+                ))}
+              </div>
+              <TotalDisplay total={calculateTotal()} />
+              <p className="text-center text-sm text-gray-600 mb-6">
+                Seu pedido ficará reservado até as 17:00
+              </p>
+              <div className="flex justify-center">
+                <Button
+                  variant="btnBrown"
+                  className="rubik-600 w-full md:w-[420px] h-[64px]"
+                  onClick={handleReservation}
+                >
+                  Fazer Reserva
+                </Button>
+              </div>
             </div>
-            <TotalDisplay total={calculateTotal()} />
-            <p className="text-center text-sm text-gray-600 mb-6">
-              Seu pedido ficará reservado até as 17:00
-            </p>
-            <div className="flex justify-center">
-              <Button
-                variant="btnBrown"
-                className="rubik-600 w-full md:w-[420px] h-[64px]"
-                onClick={handleReservation}
-              >
-                Fazer Reserva
-              </Button>
-            </div>
-          </div>
-        ) : (
-          <p className="text-center text-gray-600">Seu carrinho está vazio.</p>
-        )}
+          ) : (
+            <p className="text-center text-gray-600">Seu carrinho está vazio.</p>
+          )}
+        </Suspense>
       </main>
       <Footer />
     </div>
