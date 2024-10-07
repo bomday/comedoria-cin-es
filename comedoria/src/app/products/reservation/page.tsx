2d3bfc520f87c0d0f4393ab3bb73d9869cfca1f0
Day Lima <dayanecamilelima@gmail.com>
2024-10-07 12:50:29 -0300

    deploy correction

--- comedoria/src/app/products/reservation/page.tsx
+++ comedoria/src/app/products/reservation/page.tsx
@@ -11,8 +11,25 @@ import { useCart } from "./sections/useCartHook/useCart";
 import { Suspense } from "react"; // Importando Suspense
 import { useSearchParams } from "next/navigation"; // Importando useSearchParams
 
-const FinalizeReservation = () => {
+const CartContent = () => {
   const { cartItems, calculateTotal } = useCart();
+
+  return (
+    <div className="flex flex-col mx-auto max-w-full md:max-w-[640px]">
+      <div className="space-y-4 mb-4">
+        {cartItems.map(item => (
+          <CartItemDisplay key={item.product_name} item={item} />
+        ))}
+      </div>
+      <TotalDisplay total={calculateTotal()} />
+      <p className="text-center text-sm text-gray-600 mb-6">
+        Seu pedido ficará reservado até as 17:00
+      </p>
+    </div>
+  );
+};
+
+const FinalizeReservation = () => {
   const router = useRouter();
   const searchParams = useSearchParams(); // Aqui você usa useSearchParams
 
@@ -31,31 +48,17 @@ const FinalizeReservation = () => {
           Finalize sua reserva
         </h2>
         <Suspense fallback={<div>Loading...</div>}> {/* Boundary de Suspense */}
-          {cartItems.length > 0 ? (
-            <div className="flex flex-col mx-auto max-w-full md:max-w-[640px]">
-              <div className="space-y-4 mb-4">
-                {cartItems.map(item => (
-                  <CartItemDisplay key={item.product_name} item={item} />
-                ))}
-              </div>
-              <TotalDisplay total={calculateTotal()} />
-              <p className="text-center text-sm text-gray-600 mb-6">
-                Seu pedido ficará reservado até as 17:00
-              </p>
-              <div className="flex justify-center">
-                <Button
-                  variant="btnBrown"
-                  className="rubik-600 w-full md:w-[420px] h-[64px]"
-                  onClick={handleReservation}
-                >
-                  Fazer Reserva
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <p className="text-center text-gray-600">Seu carrinho está vazio.</p>
-          )}
+          <CartContent /> {/* Agora o conteúdo do carrinho está encapsulado no Suspense */}
         </Suspense>
+        <div className="flex justify-center">
+          <Button
+            variant="btnBrown"
+            className="rubik-600 w-full md:w-[420px] h-[64px]"
+            onClick={handleReservation}
+          >
+            Fazer Reserva
+          </Button>
+        </div>
       </main>
       <Footer />
     </div>
